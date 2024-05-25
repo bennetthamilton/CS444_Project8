a64be7b1f7ad3be896db2c3e4482fae99f2d48a6
bennetthamilton <bennetthamilton0@gmail.com>
2024-05-25 12:29:33 -0700

    add function definitions

--- dir.h
+++ dir.h
@@ -3,7 +3,12 @@
 
 #define ENTRY_SIZE 32   // 2 bytes for inode number, 16 bytes for name, 14 bytes reserved
 
+struct directory;
+struct directory_entry;
 struct directory *directory_open(int inode_num);
+int directory_get(struct directory *dir, struct directory_entry *ent);
+void directory_close(struct directory *d);
+void ls(void);
 void mkfs(void);
 
 #endif
