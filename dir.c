1ef4b03d078f000b7bdbcaca9385e4e4bf64c3ad
bennetthamilton <bennetthamilton0@gmail.com>
2024-05-25 11:55:02 -0700

    implement mkfs

--- dir.c
+++ dir.c
@@ -8,6 +8,12 @@ struct directory {
     unsigned int offset;
 };
 
+struct direntry {
+    unsigned int inode_num; // inode number of the file 2 bytes
+    char name[16];          // name of the file 16 bytes
+    char reserved[14];      // 14 reserved bytes
+};
+
 struct directory *directory_open(int inode_num){
     // Use iget() to get the inode for this file
     struct inode *in = iget(inode_num);
@@ -26,4 +32,39 @@ struct directory *directory_open(int inode_num){
 
     // Return the pointer to the struct directory
     return dir;
+}
+
+void mkfs(void){
+    // Get new inode with ialloc
+    struct inode *in = ialloc();
+
+    // Call alloc to get a new data block
+    int block_num = alloc();
+
+    // Initialize the inode returned from ialloc
+    // flags = 2, size = byte size of directory (64 bytes, 2 entries), block_ptr[0] needs to point to the block we got from alloc
+    in->flags = 2;
+    in->size = 2 * sizeof(struct directory);
+    in->block_ptr[0] = block_num;
+
+    // Make unsigned char block[BLOCK_SIZE] that you can populate with new directory data
+    unsigned char block[BLOCK_SIZE];
+    memset(block, 0, BLOCK_SIZE);
+
+    // Add the directory entries
+    // First entry: inode number of the directory itself, name is "."
+    // Second entry: inode number of the parent directory, name is ".."
+    struct direntry *entry1 = (struct directory *)block;
+    entry1->inode_num= in->inode_num;
+    entry1->name[0] = '.';
+
+    struct direntry *entry2 = (struct directory *)(block + sizeof(struct directory));
+    entry2->inode_num = in->inode_num;
+    entry2->name[0] = '..';
+
+    // Write the block to the disk with bwrite
+    bwrite(block_num, block);
+
+    // call iput() to write the new direcory inode out to disk and free up the in-core inode
+    iput(in);
 }
--- dir.h
+++ dir.h
@@ -2,5 +2,6 @@
 #define DIR
 
 struct directory *directory_open(int inode_num);
+void mkfs(void);
 
 #endif
