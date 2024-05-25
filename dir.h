8e5d35d310e4d0fb6e34db987cd4637e736d2670
bennetthamilton <bennetthamilton0@gmail.com>
2024-05-25 12:04:09 -0700

    add constant for entry size

--- dir.h
+++ dir.h
@@ -1,6 +1,8 @@
 #ifndef DIR
 #define DIR
 
+#define ENTRY_SIZE 32   // 2 bytes for inode number, 16 bytes for name, 14 bytes reserved
+
 struct directory *directory_open(int inode_num);
 void mkfs(void);
 
